29d481744f6adbf780daadd7795f888701b57a31
Genebrazley23 <166544100+Genebrazley23@users.noreply.github.com>
2024-11-09 09:00:46 -0600

    corrections

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const userRoutes = require("./users");
 const clothingItem = require("./clothingItems");
+
 const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRoutes);
